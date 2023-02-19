9411fb3f95db6c21c6a9b5de716a86c5ce62dfff
Ricardo Silva <ricardords2002@hotmail.com>
2023-02-19 14:56:01 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// criar var
+// var x = prompt("Qual o seu nome?");
+// alert ("o seu nome é "+x);
+
+// document.write("Teste");
+// document.write("<img src='http://www.google.com.br/google.jpg' />");
+
+// Criar function
+// function trocarDiv(nome, idade) {
+// 	var area = document.getElementById('area');
+// 	var texto = prompt("Qual seu Sobrenome?");
+
+// 	area.innerHTML = nome+" "+texto+" tem "+idade+" anos";	
+// }
+
+// acrescentar itens na lista
+ // function adicionarIngrediente(){
+ // 	var ing = document.getElementById("ingrediente").value;
+// 	var listahtml = document.getElementById("lista").innerHTML;
+
+ // 	listahtml = listahtml + "<li>"+ing+"</li>";
+// 	document.getElementById("lista").innerHTML = listahtml;
+ // }
+
